a544c6ad31a8938fe2dc4546d15407982bf02866
Jean-Marie GERONIMI <geronimi@goodbarber.com>
2023-10-19 17:58:27 +0200

    add skills templates

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,9 +1,11 @@
+from pyexpat import model
 from django.db import models
 
 
 class Skill(models.Model):
     name = models.CharField(max_length=255)
     image = models.ImageField(upload_to="portfolio/static/img/logos/", blank=True)
+    percent = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,13 @@
+from multiprocessing import context
 from django.shortcuts import render
 
+from . import models
+
 def index(request):
-    return render(request, './portfolio/index.html')
+    if request.method == 'POST':
+        pass
+    skills = models.Skill.objects.all()
+    projects = models.Project.objects.all()
+    context = { "skills": skills, "projects": projects }
+
+    return render(request, './portfolio/index.html', context=context)
