eff7e10f376d25ab2eca57dcbd40650dcfbea66e
Jm GERONIMI <spawn2x@protonmail.com>
2024-02-09 01:13:41 +0100

    update db

--- app/settings.py
+++ app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-import portfolio
+from common.config.config_prod import Config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -91,12 +91,12 @@ if Config.IS_POSTGRESQL_BACKEND:
         }
     }
 else:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
