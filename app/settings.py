ff2b5542ed05baf2ce1253a71729cdfa6a1db0a8
Jm GERONIMI <spawn2x@protonmail.com>
2024-02-09 01:28:45 +0100

    update db dir

--- app/settings.py
+++ app/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'portfoliov2',
+#         'USER': 'portfoliov2',
+#         'PASSWORD': 'portfoliov2',
+#         'HOST': '172.17.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'portfoliov2',
-        'USER': 'portfoliov2',
-        'PASSWORD': 'portfoliov2',
-        'HOST': '172.17.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': BASE_DIR / 'db.sqlite3',
-    #     }
-    # }
 
 
 # Password validation
