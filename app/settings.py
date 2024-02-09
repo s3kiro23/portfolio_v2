ff6d238f2e4a041c7cd6b74e82225c2d0562a69f
Jm GERONIMI <spawn2x@protonmail.com>
2024-02-09 01:19:01 +0100

    update db

--- app/settings.py
+++ app/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from common.config.config_prod import Config
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,24 +76,23 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if Config.IS_POSTGRESQL_BACKEND:
-    DATABASES = {
-        'default': {
-            'ENGINE': Config.PG_DATABASE_ENGINE,
-            'NAME': Config.PG_DATABASE_NAME,
-            'USER': Config.PG_DATABASE_USER,
-            'PASSWORD': Config.PG_DATABASE_PASSWORD,
-            'HOST': Config.PG_DATABASE_HOST,
-            'PORT': Config.PG_DATABASE_PORT,
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfoliov2',
+        'USER': 'portfoliov2',
+        'PASSWORD': 'portfoliov2',
+        'HOST': '172.17.0.1',
+        'PORT': '5432',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
+
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     }
+    # }
 
 
 # Password validation
